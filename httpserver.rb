310a0c27dfee4c8bd2766dd24bded58b01a03630
Chris Mytton <chrismytton@gmail.com>
2016-04-01 18:49:44 +0200

    Use SHA256 in httpserver formula

--- httpserver.rb
+++ httpserver.rb
@@ -3,7 +3,7 @@ require "formula"
 class Httpserver < Formula
   homepage "https://github.com/chrismytton/httpserver"
   url "https://github.com/chrismytton/httpserver/releases/download/v0.0.1/httpserver-darwin-x64-v0.0.1.tar.gz"
-  sha1 "33df17c4e169b585aeb49acf333ffb421f9f2de8"
+  sha256 "7f172d4b0c55e99576e010b6de6e13a3336cd8478f0a0bb02dd9ed64714b0379"
 
   def install
     bin.install 'httpserver'
