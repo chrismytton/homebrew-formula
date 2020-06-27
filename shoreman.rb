eb68596a4990af2dcbc35a042ab58b2e8cef273b
Chris Mytton <mail@chrismytton.uk>
2020-06-27 23:49:36 +0100

    [Shoreman] Add v1.0.0

--- shoreman.rb
+++ shoreman.rb
@@ -2,6 +2,10 @@ require 'formula'
 
 class Shoreman < Formula
   homepage 'https://github.com/chrismytton/shoreman'
+
+  url 'https://github.com/chrismytton/shoreman/archive/v1.0.0.tar.gz'
+  sha256 'fc9b5bff73ff013a16f63a55d4843b1d4e8243537fea7ebbb6e5e8a762f04a31'
+
   head 'https://github.com/chrismytton/shoreman.git'
 
   def install
